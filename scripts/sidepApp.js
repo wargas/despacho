4f088b27790f683b30dc224d161b8a1668398ba2
Wargas <teixeira.wargas@gmail.com>
2023-05-15 06:29:19 -0300

    especie

--- scripts/sidepApp.js
+++ scripts/sidepApp.js
@@ -15,29 +15,29 @@ app.controller('spCtrl', function ($scope, $filter, $cookies) {
 
     //Seção do Benefício
     $scope.especies = [
-        { codigo: '21', descricao: 'Pensão por Morte Urbana' },
-        { codigo: '21', descricao: 'Pensão por Morte Rural' },
+        { codigo: '21u', descricao: 'Pensão por Morte Urbana' },
+        { codigo: '21r', descricao: 'Pensão por Morte Rural' },
         { codigo: '93', descricao: 'Pensão por Morte por Acidente de Trabalho' },
         { codigo: '25', descricao: 'Auxílio-Reclusão Urbano' },
         { codigo: '25', descricao: 'Auxílio-Reclusão Rural' },
-        { codigo: '31', descricao: 'Benefício por Incapacidade Temporária Urbano' },
-        { codigo: '31', descricao: 'Benefício por Incapacidade Temporária Rural' },
+        { codigo: '31u', descricao: 'Benefício por Incapacidade Temporária Urbano' },
+        { codigo: '31r', descricao: 'Benefício por Incapacidade Temporária Rural' },
         { codigo: '36', descricao: 'Auxílio-Acidente Previdenciário' },
         { codigo: '94', descricao: 'Auxílio-Acidente por Acidente de Trabalho' },
-        { codigo: '41', descricao: 'Aposentadoria por Idade Urbana' },
-        { codigo: '41', descricao: 'Aposentadoria por Idade Rural' },
+        { codigo: '41u', descricao: 'Aposentadoria por Idade Urbana' },
+        { codigo: '41r', descricao: 'Aposentadoria por Idade Rural' },
         { codigo: '42', descricao: 'Aposentadoria por Tempo de Contribuição' },
         { codigo: '46', descricao: 'Aposentadoria Especial' },
         { codigo: '57', descricao: 'Aposentadoria Especial do Professor' },
-        { codigo: '80', descricao: 'Salário-Maternidade Urbano' },
-        { codigo: '80', descricao: 'Salário-Maternidade Rural' },
+        { codigo: '80u', descricao: 'Salário-Maternidade Urbano' },
+        { codigo: '80r', descricao: 'Salário-Maternidade Rural' },
         { codigo: '87', descricao: 'Benefício Assistencial ao Deficiente' },
         { codigo: '88', descricao: 'Benefício Assistencial ao Idoso' },
         { codigo: 'CTC', descricao: 'Certidão de Tempo de Contribuição' }
     ];
     $scope.tipoIdentificador = "Benefício";
     $scope.tamanhoIdentificador = 13;
-    $scope.especieSelecionada = especie === '80' ?  $scope.especies[15] : $scope.especies[10];
+    $scope.especieSelecionada = $scope.especies.find(e => e.codigo == especie) || especie[15]
     $scope.especieExibicao = "E/NB";
     $scope.numeroBeneficio = new URLSearchParams(location.search).get('nb');
     $scope.numeroBeneficioValido = false;
@@ -131,7 +131,7 @@ app.controller('spCtrl', function ($scope, $filter, $cookies) {
     };
     //Campos do(s) Vínculo(s) Empregatício(s)
     $scope.itensVinculo = {
-        chkVinculo: false,
+        chkVinculo: true,
         chkDocCTPS: false,
         chkDocFRE: false,
         chkDocContrato: false,
